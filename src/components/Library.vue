c6936a1f32d9d101f83219af33fe9eb70bb04c7c
Seredin Anton <seredin.anton2014@gmail.com>
2022-10-13 23:33:53 +0300

    Seredin library initial commit

---
+++ src/components/Library.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="seredin-library">
+    <div>
+      <h1>Seredin programmer library</h1>
+      <p>Как водится каждый раз начинаю писать новую библиотеку с кодом :-) посмотрим что выйдет на этот раз</p>
+      <p>Идея в том что теперь будет множество раздвижных и всплывающих окон и множество ссылок на GIT с описанием использования.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+</style>

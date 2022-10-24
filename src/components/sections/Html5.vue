359836e79960eeb19fc51346139e2e30fc8f022f
Seredin Anton <seredin.anton2014@gmail.com>
2022-10-25 02:15:02 +0530

    HTML5 section Improvement

--- src/components/sections/Html5.vue
+++ src/components/sections/Html5.vue
@@ -1,18 +1,65 @@
 <template>
+  <h3>Have to know</h3>
   <ul>
     <li>&lt;article&gt;</li>
     <li>&lt;aside&gt;</li>
     <li>&lt;details&gt;</li>
+    <li>&lt;summary&gt;</li>
     <li>&lt;figcaption&gt;</li>
     <li>&lt;figure&gt;</li>
     <li>&lt;footer&gt;</li>
     <li>&lt;header&gt;</li>
+    <li>&lt;hgroup&gt;</li>
     <li>&lt;main&gt;</li>
     <li>&lt;mark&gt;</li>
     <li>&lt;nav&gt;</li>
     <li>&lt;section&gt;</li>
-    <li>&lt;summary&gt;</li>
     <li>&lt;time&gt;</li>
+    <li>&lt;details&gt;</li>
+    <li>&lt;dialog&gt;</li>
+    <li>&lt;embed&gt;</li>
+    <li>&lt;mark&gt;</li>
+    <li>&lt;menuitem&gt;</li>
+    <li>&lt;svg&gt;</li>
+    <li>&lt;template&gt;</li>
+  </ul>
+  <h3>Rare</h3>
+  <ul>
+    <li>&lt;audio&gt;</li>
+    <li>&lt;source&gt;</li>
+    <li>&lt;track&gt;</li>
+    <li>&lt;figure&gt;</li>
+    <li>&lt;figcaption&gt;</li>
+    <li>&lt;keygen&gt;</li>
+    <li>&lt;meter&gt;</li>
+    <li>&lt;picture&gt;</li>
+    <li>&lt;progress&gt;</li>
+    <li>&lt;video&gt;</li>
+    <li>&lt;wbr&gt;</li>
+  </ul>
+  <h3>Never used</h3>
+  <ul>
+    <li>&lt;&gt;</li>
+    <li>&lt;bdi&gt;</li>
+    <li>&lt;data&gt;</li>
+    <li>&lt;datalist&gt;</li>
+  </ul>
+  <h3>not HTML5</h3>
+  <ul>
+    <li>&lt;abbr&gt;</li>
+    <li>&lt;address&gt;</li>
+    <li>&lt;area&gt;</li>
+    <li>&lt;base&gt;</li>
+    <li>&lt;bdo&gt;</li>
+    <li>&lt;blockquote&gt;</li>
+    <li>&lt;code&gt;</li>
+    <li>&lt;cite&gt;</li>
+    <li>&lt;dfn&gt;</li>
+    <li>&lt;ins&gt;</li>
+    <li>&lt;menu&gt;</li>
+    <li>&lt;param&gt;</li>
+    <li>&lt;optgroup&gt;</li>
+    <li>&lt;small&gt;</li>
   </ul>
 </template>
 

55081977189ea6d0b6f1b195f5733b898bbdc788
Seredin Anton <seredin.anton2014@gmail.com>
2022-11-24 14:35:55 +0530

    Express Vue CRUD system

--- src/components/sections/ExpressAndVue.vue
+++ src/components/sections/ExpressAndVue.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>Express and VUE</div>
+  <article>
+    <h2>Express и VUE с MongoDB</h2>
+
+    <section>
+      Связывание crud системы VUE и Express
+      <ul>
+        <li>наверное надо будет придумать описание попозже, (много ерунды тяжело за неё взяться)</li>
+        <li><a href="https://github.com/NinetiesFiasco/express-vue-mongo-crud" target="_blank">Репозиторий</a></li>
+        <li><CodeBlock>git clone https://github.com/NinetiesFiasco/express-vue-mongo-crud.git my-vue-express-crud</CodeBlock></li>
+      </ul>
+    </section>
+
+  </article>
 </template>
 
 <script>
+import {CodeBlock} from './CurrentLibrary'
 export default {
-  name: 'ExpressVue'
+  name: 'ExpressVue',
+  components: {
+    CodeBlock
+  }
 }
 </script>
 
